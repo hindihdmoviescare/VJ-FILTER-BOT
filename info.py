ed13c4557430c3d84b3804a9c12515deac4066ce
hindihdmoviescare <hindihdmoviescare@gmail.com>
2025-05-23 21:48:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://vault.pictures/p/47a90da60ea84784bd782f5e334c5acb')).split()
 
 
 # Admins & Users
